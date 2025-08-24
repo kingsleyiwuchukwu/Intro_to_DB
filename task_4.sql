bb6194b85dcb6a3c2868e20f8fbfe9a0e9824183
kingsleyiwuchukwu <kingsleyiwuchukwu0@gmail.com>
2025-08-24 22:39:01 +0100

    script that prints the full description of the table books from the database alx_book_store in your MySQL server.

---
+++ task_4.sql
@@ -0,0 +1,9 @@
+
+-- Switch to the alx_book_store database
+USE alx_book_store;
+
+-- Get full description of the books table without using DESCRIBE or EXPLAIN
+SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'books';
