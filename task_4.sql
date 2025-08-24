6d919582373d0674405e1a6aa32b5483f789e6e4
kingsleyiwuchukwu <kingsleyiwuchukwu0@gmail.com>
2025-08-24 22:52:41 +0100

    Updated task 4

--- task_4.sql
+++ task_4.sql
@@ -2,8 +2,8 @@
 -- Switch to the alx_book_store database
 USE alx_book_store;
 
--- Get full description of books table
+-- Get full description of Books table
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'
-  AND TABLE_NAME = 'books';
+  AND TABLE_NAME = 'Books';
